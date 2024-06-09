3ea53114f61c4cc28a9aea76f691e42571df58cb
MariaTech2024 <mariavolzhina@gmail.com>
2024-06-10 00:08:55 +0300

    backend

--- server/index.js
+++ server/index.js
@@ -2,37 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import path from "path";
-import { fileURLToPath } from 'url';
 import userRouter from './routes/user.js';
 import questionRouter from './routes/question.js';
 import answerRouter from './routes/answer.js';
 
-// Load environment variables from .env file
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Create an Express application
 const app = express();
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
 // Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware to parse cookies
+
 app.use(cookieParser());
 
-// Enable Cross-Origin Resource Sharing (CORS)
-app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-  credentials: true // Allow cookies to be sent cross-origin
-}));
+
+app.use(cors());
 
 
 app.use('/users', userRouter);
@@ -42,12 +29,4 @@ app.use('/answers', answerRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Run on ${process.env.PORT || 5000}`);
-});
-
-// Have Node serve the files for our built React app
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
